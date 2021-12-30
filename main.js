0d43671422e22d232c9e2a4c1b2354e7d00a609b
mlukyanenko <m.lukyanenko@dunice.net>
2021-12-30 17:35:37 +0300

    step5

--- main.js
+++ main.js
@@ -15,33 +15,50 @@ $(document).ready(function() {
 
   const createHtml = (todo) => {
     return ` <div class="flex">
-              <input class="elem-input" data-id="${todo.id}" type="checkbox">
+              <input class="elem-input" data-id="${todo.id}" type="checkbox" ${todo.checked ? "checked" : ""}>
               <div class="container" data-id="${todo.id}">${todo.description}</div>
               <button data-id="${todo.id}" class="close">x</button>
             </div>`
   };
 
+  const checkTodo = () => {
+    let check = true;
+    const mainCheck = $('.main')[0];
+    todos.forEach((elem) => {
+      check = check && elem.checked;
+    });
+    if(check){
+      mainCheck.checked = true;
+    }else{
+      mainCheck.checked = false;
+    }
+  }
+
   const isChecked = (event) => {
     const id = Number(event.target.dataset.id);
     todos.forEach((elem) => {
       if (id === elem.id) {
-        elem.checked = true;
+        if(event.target.checked){
+          elem.checked = true;
+
+        } else {
+          elem.checked = false;
+        }
       }
     });
-    console.log(todos);
+    checkTodo();
+    render(todos);
   };
-
-  
 
   const render = (arr) => {
     let result = '';
-      $('.container').html(result);
       arr.forEach((elem) => {
         result += createHtml(elem);
       });
       $('.container').html(result);
       $('.close').click(deleteTodo);
       $('.elem-input').click(isChecked);
+      count();
      };
 
   const addTodo = () => {
@@ -61,23 +78,54 @@ $(document).ready(function() {
   const deleteTodo = (event) => {
     const id = Number(event.target.dataset.id);
     todos.forEach((elem, index) => {
-      if (id == elem.id) {
+      if (id === elem.id) {
         todos.splice(index, 1);
       }
     });
     render(todos);
   };
 
-  const checkAll = () => {
-
-
+  const checkAll = (event) => {
+    todos.forEach((elem, index) => {
+     if(event.target.checked){
+      elem.checked = true;
+     } else {
+      elem.checked = false;
+     }
+    });
+    render(todos);
   };
 
   $('.btn').click(addTodo);
   $('#text').keydown(checkEnter);
-  $('.chall').click((event) => {
-    $('.elem-input').each((index, elem) => {
-      elem.setAttribute('checked', true);
+  $('.main').click(checkAll);
+
+  const count = () => {
+    let active = 0;
+    let done = 0;
+    todos.forEach((elem) => {
+      if(elem.checked) {
+        done++;
+      } else {
+        active++;
+      }
     });
-  });
+    inner(active, done);
+    
+  };
+  
+  const createHtmlTask = (active, done) => {
+    return `<div class="total-tasks">Total tasks:${todos.length}</div>
+            <div class="active-tasks">Active tasks:${active}</div>
+            <div class="completed-tasks">Completed tasks:${done}</div>  
+	         `
+  };
+
+  const inner = (active, done) => {
+    let result = '';
+    result = createHtmlTask(active, done);
+    $('.tasks-counter').html(result);
+  };
+
 });
+
